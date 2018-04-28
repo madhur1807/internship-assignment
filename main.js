12842577e8d5307a8ae227f38b5e27c6d0933065
madhur1807 <madhurmittal1807@gmail.com>
2018-04-28 11:25:04 +0530

    ..

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ function displayActive(){
 	var number = document.getElementById("myUL").getElementsByTagName("li");
 	var checked = 0;
 	var unchecked = 0;
-	for(i=0; i<number.length; i++){		
+	for(let i=0; i<number.length; i++){		
 		if(number[i].classList.contains("checked")){
 			checked++;
 		} else{
